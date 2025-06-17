2b687247c5febeaa93d092fd313a294b2703c3a5
ivan <ivanroaprieto@gmail.com>
2025-06-17 13:01:27 +0200

    Initial Commit

---
+++ triviaa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  triviaa
+//
+//  Created by Mañanas on 17/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
